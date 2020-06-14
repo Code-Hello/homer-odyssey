366109d7f61c13730835167616b10b05a49e69e8
Code-Hello <m.tuto@protonmail.com>
2020-06-14 21:04:24 +0200

    form done

--- front/front-repo/src/components/SignUp.jsx
+++ front/front-repo/src/components/SignUp.jsx
@@ -5,27 +5,64 @@ class SignUp extends React.Component {
     super(props);
     this.state = {
       email: '',
+      password: '',
+      passwordbis: '',
+      name: '',
+      lastname: '',
     };
-    this.updateEmailField = this.updateEmailField.bind(this);
+    this.updateField = this.updateField.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  updateEmailField(event) {
+  updateField(event) {
     this.setState({
-      email: event.target.value,
+      [event.target.name]: event.target.value,
     });
   }
 
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
+  }
+
   render() {
-    const { email } = this.state;
+    const { email, password, passwordbis, name, lastname } = this.state;
     return (
       <div>
-        <h1>{email}</h1>
-        <input
-          type="email"
-          name="email"
-          onChange={this.updateEmailField}
-          value={email}
-        />
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            onChange={this.updateField}
+            value={email}
+          />
+          <input
+            type="password"
+            name="password"
+            onChange={this.updateField}
+            value={password}
+          />
+          <input
+            type="password"
+            name="passwordbis"
+            onChange={this.updateField}
+            value={passwordbis}
+          />
+          <input
+            type="text"
+            name="name"
+            onChange={this.updateField}
+            value={name}
+          />
+          <input
+            type="text"
+            name="lastname"
+            onChange={this.updateField}
+            value={lastname}
+          />
+          <input type="submit" value="Soumettre" />
+        </form>
       </div>
     );
   }
