a2a6185a7ba2c46d86c8903913ad4ae0ace94b56
Code-Hello <m.tuto@protonmail.com>
2020-06-14 23:41:34 +0200

    database added

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,8 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const connection = require("../../helpers/db");
 
-router.post('/signup', function(req, res, next) {
-    res.send('I am in POST signup');
+router.post("/signup", function(req, res, next) {
+    const user = req.body;
+    connection.query("INSERT INTO users SET ?", user, function(
+        error,
+        results,
+        fields
+    ) {
+        if (error) res.status(500).end();
+        res.end();
+    });
 });
 
 module.exports = router;
