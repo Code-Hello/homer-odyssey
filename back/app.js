be1359af777fd55bbdda0277a7b4a8dc5e021ba0
Code-Hello <m.tuto@protonmail.com>
2020-06-14 22:47:13 +0200

    express router done

---
+++ back/app.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+const authRouter = require("./routes/auth/auth");
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.static(__dirname + "/public"));
+
+app.use("/auth", authRouter);
+
+app.get("/", (req, res) => {
+    res.send("youhou");
+});
+/// dans le cas d'une route non trouvée, je retourne le code 404 'Not Found'
+app.use(function(req, res, next) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+//je lance le serveur node
+let server = app.listen(process.env.PORT || 8000, function() {
+    console.log("Listening on port " + server.address().port);
+});
