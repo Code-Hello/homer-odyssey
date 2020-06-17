4a5e7bbf4cf9f3382234fc47e65f0fabbd6cdafa
Code-Hello <m.tuto@protonmail.com>
2020-06-17 19:00:19 +0200

    style added with material ui

--- front/front-repo/src/App.jsx
+++ front/front-repo/src/App.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { Grid, Paper } from '@material-ui/core';
+
 import './App.css';
 
 import SignUp from './components/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <SignUp />
-    </div>
+    <MuiThemeProvider>
+      <Grid container alignItems="center" style={{ height: '100%' }}>
+        <Grid item xs={12}>
+          <Paper elevation={4} style={{ margin: 32 }}>
+            <Grid container alignItems="center" justify="center">
+              <Grid item xs={12} sm={6} style={{ 'text-align': 'center' }}>
+                <img
+                  src="http://images.innoveduc.fr/react_odyssey_homer/wildhomer.png"
+                  alt="Homer Simpson"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} alignContent="center">
+                <SignUp />
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </MuiThemeProvider>
   );
 }
 
--- front/front-repo/src/components/SignUp.jsx
+++ front/front-repo/src/components/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, TextField, Snackbar, Grid } from '@material-ui/core';
 
 class SignUp extends React.Component {
   constructor(props) {
@@ -10,9 +11,11 @@ class SignUp extends React.Component {
       name: '',
       lastname: '',
       flash: '',
+      open: false,
     };
     this.updateField = this.updateField.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
 
   updateField(event) {
@@ -36,51 +39,106 @@ class SignUp extends React.Component {
       })
         .then((res) => res.json())
         .then(
-          (res) => this.setState({ flash: res.flash }),
-          (err) => this.setState({ flash: err.flash })
+          (res) => this.setState({ flash: res.flash, open: true }),
+          (err) => this.setState({ flash: err.flash, open: true })
         );
     }
   }
 
+  handleClose() {
+    this.setState({ open: false });
+  }
+
   render() {
-    const { email, password, passwordbis, name, lastname } = this.state;
+    const {
+      email,
+      password,
+      passwordbis,
+      name,
+      lastname,
+      flash,
+      open,
+    } = this.state;
     return (
-      <div>
-        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            onChange={this.updateField}
-            value={email}
-          />
-          <input
-            type="password"
-            name="password"
-            onChange={this.updateField}
-            value={password}
-          />
-          <input
-            type="password"
-            name="passwordbis"
-            onChange={this.updateField}
-            value={passwordbis}
-          />
-          <input
-            type="text"
-            name="name"
-            onChange={this.updateField}
-            value={name}
-          />
-          <input
-            type="text"
-            name="lastname"
-            onChange={this.updateField}
-            value={lastname}
-          />
-          <input type="submit" value="Soumettre" />
-        </form>
-      </div>
+      <Grid container alignItems="center" style={{ padding: 15 }}>
+        <Grid item>
+          {JSON.stringify(this.state, 1, 1)}
+          <h2>Sign up !</h2>
+          <form onSubmit={this.handleSubmit}>
+            <TextField
+              fullWidth
+              required
+              type="email"
+              name="email"
+              label="Email"
+              placeholder="your@mail.com"
+              onChange={this.updateField}
+              value={email}
+            />
+            <TextField
+              fullWidth
+              required
+              type="password"
+              name="password"
+              label="Password"
+              onChange={this.updateField}
+              value={password}
+            />
+            <TextField
+              fullWidth
+              required
+              type="password"
+              name="passwordbis"
+              label="Password Copy"
+              onChange={this.updateField}
+              value={passwordbis}
+            />
+            <TextField
+              fullWidth
+              required
+              type="text"
+              name="name"
+              label="Name"
+              placeholder="James"
+              onChange={this.updateField}
+              value={name}
+            />
+            <TextField
+              fullWidth
+              required
+              type="text"
+              name="lastname"
+              label="Lastname"
+              placeholder="Bond"
+              onChange={this.updateField}
+              value={lastname}
+            />
+            <Grid
+              container
+              item
+              xs={12}
+              justify="flex-end"
+              style={{ padding: 20 }}
+            >
+              <Grid item>
+                <Button variant="contained" color="primary" type="submit">
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+            <Snackbar
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+              }}
+              open={open}
+              autoHideDuration={5000}
+              onClose={this.handleClose}
+              message={flash}
+            />
+          </form>
+        </Grid>
+      </Grid>
     );
   }
 }
